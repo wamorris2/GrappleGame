7620c38877f3f25a3bf74680fa28ffa62996b73a
WillemAMorris <wamorris278@gmail.com>
2022-02-13 23:26:36 -0500

    added Player script for horizontal movement and jumping once in the air

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D checkGrounded;
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private float movingX;
+    [SerializeField] private bool jumped;
+    [SerializeField] private bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            if(grounded)
+                jumped = true;
+        }
+        movingX = Input.GetAxis("Horizontal");
+    }
+
+    // FixedUpdate is called once every physics update
+    private void FixedUpdate()
+    {
+        Collider2D[] collisions = new Collider2D[2];
+        int numOfCollisions = checkGrounded.OverlapCollider(new ContactFilter2D(), collisions);
+        if (numOfCollisions > 1)
+            grounded = true;
+        else
+            grounded = false;
+        if (grounded && jumped)
+        {
+            rigidBody.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+            jumped = false;
+        }
+        rigidBody.velocity = new Vector2(movingX * 3, rigidBody.velocity.y);
+    }
+}
